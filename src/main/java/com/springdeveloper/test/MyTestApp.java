68eb8c31dcdf70bc838e813e41b986d4cab662ab
Thomas Risberg <trisberg@pivotal.io>
2017-04-21 12:32:32 -0400

    Simple sample

---
+++ src/main/java/com/springdeveloper/test/MyTestApp.java
@@ -0,0 +1,11 @@
+package com.springdeveloper.test;
+
+import org.springframework.core.SpringVersion;
+
+public class MyTestApp {
+
+	public static void main(String[] args) {
+			System.out.println("Hello World!");
+			System.out.println("Spring version" + SpringVersion.getVersion());
+	}
+}
